c3a982509f9e032ec8e2428d7af82ebbd2d1f761
Tsega <tsega207@gmail.com>
2024-10-19 23:04:28 -0400

    Navbar edit

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -195,7 +195,7 @@ const AdminDashboard: React.FC = () => {
         <div className="flex flex-col h-screen bg-gray-100">
             {/* Navbar */}
             <nav className="bg-white shadow-md">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="mx-auto px-4">
                     <div className="flex justify-between h-16">
                         <div className="flex">
                             <div className="flex-shrink-0 flex items-center justify-start">
